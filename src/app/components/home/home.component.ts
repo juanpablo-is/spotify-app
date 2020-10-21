db327b0c2b214e23804e970a76ca5cd7c6a86128
Juan Pablo Barrera <juanpablo.is@outlook.com>
2020-10-20 20:25:03 -0500

    Home page artists

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  releases: any[] = [];
 
-  ngOnInit(): void {
+  constructor(private spotify: SpotifyService) {
+    this.spotify.getNewReleases()
+      .subscribe((response: any) => {
+        this.releases = response;
+      });
   }
 
 }
