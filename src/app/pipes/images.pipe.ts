c609c53d1b995bdca575fe15997a08df064629b5
Juan Pablo Barrera <juanpablo.is@outlook.com>
2020-10-20 20:49:55 -0500

    Create pipe for images and use it

--- src/app/pipes/images.pipe.ts
+++ src/app/pipes/images.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImagesPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any[]): unknown {
+    var url = value[0];
+    return url ? url.url : "https://cdn11.bigcommerce.com/s-hcp6qon/stencil/e7115e30-d125-0138-2c47-0242ac110007/icons/icon-no-image.svg";
   }
 
 }
