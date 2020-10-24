655d9c4233f85318acbd990ffe51f28d46150898
Juan Pablo Barrera <juanpablo.is@outlook.com>
2020-10-24 16:18:04 -0500

    Get endpoints artist

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
   styleUrls: ['./artist.component.css']
 })
-export class ArtistComponent implements OnInit {
+export class ArtistComponent {
 
-  constructor() { }
+  artist: any = {};
+  albums: any = [];
 
-  ngOnInit(): void {
+  constructor(private spotify: SpotifyService, private activatedRoute: ActivatedRoute) {
+
+    this.activatedRoute.params.subscribe(params => {
+      let id = params.id;
+
+      this.spotify.getArtist(id)
+        .subscribe(data => {
+          this.artist = data;
+          this.artist.url = this.artist['images'][0].url;
+        });
+
+      this.spotify.getAlbumsByArtist(id)
+        .subscribe(data => {
+          data.splice(7);
+          this.albums = data;
+        });
+    });
+
   }
-
 }
